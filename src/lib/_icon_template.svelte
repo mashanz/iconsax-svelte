a77d3de484ceb7310b86832ef3f0b7acec70ccdd
Mashanz <mangatkk@gmail.com>
2022-08-17 11:47:23 +0700

    [ADD] icons

---
+++ src/lib/_icon_template.svelte
@@ -0,0 +1,19 @@
+<script>
+	export let size = 24;
+	export let color = '#000';
+	export let variant = 'Linear';
+</script>
+
+{#if variant === 'Linear'}
+	<!-- {size} {color} -->
+{:else if variant === 'Outline'}
+	<!-- {size} {color} -->
+{:else if variant === 'Bold'}
+	<!-- {size} {color} -->
+{:else if variant === 'Bulk'}
+	<!-- {size} {color} -->
+{:else if variant === 'Broken'}
+	<!-- {size} {color} -->
+{:else if variant === 'TwoTone'}
+	<!-- {size} {color} -->
+{/if}
--- src/lib/icons.ts
+++ src/lib/icons.ts
@@ -1,9 +1,14 @@
 import I3DCubeScan from '$lib/I3DCubeScan.svelte';
+import I3DRotate from '$lib/I3DRotate.svelte';
 
 export const icons = [
 	{
 		icon: I3DCubeScan,
-		name: 'I3DCubeScan',
-		category: 'Delivery'
+		name: 'I3DCubeScan'
+	},
+	{
+		icon: I3DRotate,
+		name: 'I3DRotate',
+		category: ''
 	}
 ];
