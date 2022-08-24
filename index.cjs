6b7150481ad8a027e06b9ec6b81239d19e7ccac1
Hanjara <hanjara@landx.id>
2022-08-24 16:32:08 +0700

    [UPD] generated

--- index.cjs
+++ index.cjs
@@ -23,30 +23,42 @@ let base_content = `<script>
 {:else if variant === 'TwoTone'}
 	<!-- TwoTone -->
 {/if}
-`
+`;
 
 function read_list_files(dir) {
-    const directoryPath = path.join(__dirname, dir);
-    //passsing directoryPath and callback function
-    fs.readdir(directoryPath, function (err, files) {
-        //handling error
-        if (err) {
-            return console.log('Unable to scan directory: ' + err);
-        } 
-        //listing all files using forEach
-        files.forEach(function (file) {
-            // Do whatever you want to do with the file
-            let file_name = file.replace(".svg","").replace("-", " ").replace("-", " ").replace("-", " ");
-            file_name = file_name.split(" ");
-            file_name = file_name.map((word) => { 
-                return word[0].toUpperCase() + word.substring(1); 
-            }).join(" ");
-            file_name = file_name.replace(" ", "").replace(" ", "").replace(" ", "");
-            fs.writeFile(`./generate/${file_name}.svelte`, base_content, function (error) {
-                if (error) throw error;
-            });
-        });
-    });
+	const directoryPath = path.join(__dirname, dir);
+	//passsing directoryPath and callback function
+	fs.readdir(directoryPath, function (err, files) {
+		//handling error
+		if (err) {
+			return console.log('Unable to scan directory: ' + err);
+		}
+		//listing all files using forEach
+		files.forEach(function (file) {
+			// Do whatever you want to do with the file
+			if (file !== '.DS_Store') {
+				let file_name = file
+					.replace('.svg', '')
+					.replace('-', ' ')
+					.replace('-', ' ')
+					.replace('-', ' ');
+				file_name = file_name.split(' ');
+				file_name = file_name
+					.map((word) => {
+						return word[0].toUpperCase() + word.substring(1);
+					})
+					.join(' ');
+				file_name = file_name.replace(' ', '').replace(' ', '').replace(' ', '');
+                let first_char = file_name.substring(0,1);
+                if (!isNaN(first_char)) {
+                    file_name = "I" + file_name;
+                }
+				fs.writeFile(`./src/lib/${file_name}.svelte`, base_content, function (error) {
+					if (error) throw error;
+				});
+			}
+		});
+	});
 }
 
 read_list_files('Svg/All/linear');
--- src/lib/AddCircle.svelte
+++ src/lib/AddCircle.svelte
@@ -5,138 +5,15 @@
 </script>
 
 {#if variant === 'Linear'}
-	<svg
-		width={`${size}`}
-		height={`${size}`}
-		viewBox="0 0 24 24"
-		fill="none"
-		xmlns="http://www.w3.org/2000/svg">
-		<path
-			d="M12 22C17.5 22 22 17.5 22 12C22 6.5 17.5 2 12 2C6.5 2 2 6.5 2 12C2 17.5 6.5 22 12 22Z"
-			stroke={`${color}`}
-			stroke-width="1.5"
-			stroke-linecap="round"
-			stroke-linejoin="round" />
-		<path
-			d="M8 12H16"
-			stroke={`${color}`}
-			stroke-width="1.5"
-			stroke-linecap="round"
-			stroke-linejoin="round" />
-		<path
-			d="M12 16V8"
-			stroke={`${color}`}
-			stroke-width="1.5"
-			stroke-linecap="round"
-			stroke-linejoin="round" />
-	</svg>
+	<!-- Linear -->
 {:else if variant === 'Outline'}
-	<svg
-		width={`${size}`}
-		height={`${size}`}
-		viewBox="0 0 24 24"
-		fill="none"
-		xmlns="http://www.w3.org/2000/svg">
-		<path
-			d="M12 22.75C6.07 22.75 1.25 17.93 1.25 12C1.25 6.07 6.07 1.25 12 1.25C17.93 1.25 22.75 6.07 22.75 12C22.75 17.93 17.93 22.75 12 22.75ZM12 2.75C6.9 2.75 2.75 6.9 2.75 12C2.75 17.1 6.9 21.25 12 21.25C17.1 21.25 21.25 17.1 21.25 12C21.25 6.9 17.1 2.75 12 2.75Z"
-			fill={`${color}`} />
-		<path
-			d="M16 12.75H8C7.59 12.75 7.25 12.41 7.25 12C7.25 11.59 7.59 11.25 8 11.25H16C16.41 11.25 16.75 11.59 16.75 12C16.75 12.41 16.41 12.75 16 12.75Z"
-			fill={`${color}`} />
-		<path
-			d="M12 16.75C11.59 16.75 11.25 16.41 11.25 16V8C11.25 7.59 11.59 7.25 12 7.25C12.41 7.25 12.75 7.59 12.75 8V16C12.75 16.41 12.41 16.75 12 16.75Z"
-			fill={`${color}`} />
-	</svg>
+	<!-- Outline -->
 {:else if variant === 'Bold'}
-	<svg
-		width={`${size}`}
-		height={`${size}`}
-		viewBox="0 0 24 24"
-		fill="none"
-		xmlns="http://www.w3.org/2000/svg">
-		<path
-			d="M12 2C6.49 2 2 6.49 2 12C2 17.51 6.49 22 12 22C17.51 22 22 17.51 22 12C22 6.49 17.51 2 12 2ZM16 12.75H12.75V16C12.75 16.41 12.41 16.75 12 16.75C11.59 16.75 11.25 16.41 11.25 16V12.75H8C7.59 12.75 7.25 12.41 7.25 12C7.25 11.59 7.59 11.25 8 11.25H11.25V8C11.25 7.59 11.59 7.25 12 7.25C12.41 7.25 12.75 7.59 12.75 8V11.25H16C16.41 11.25 16.75 11.59 16.75 12C16.75 12.41 16.41 12.75 16 12.75Z"
-			fill={`${color}`} />
-	</svg>
+	<!-- Bold -->
 {:else if variant === 'Bulk'}
-	<svg
-		width={`${size}`}
-		height={`${size}`}
-		viewBox="0 0 24 24"
-		fill="none"
-		xmlns="http://www.w3.org/2000/svg">
-		<path
-			opacity="0.4"
-			d="M12 22C17.5228 22 22 17.5228 22 12C22 6.47715 17.5228 2 12 2C6.47715 2 2 6.47715 2 12C2 17.5228 6.47715 22 12 22Z"
-			fill={`${color}`} />
-		<path
-			d="M16 11.25H12.75V8C12.75 7.59 12.41 7.25 12 7.25C11.59 7.25 11.25 7.59 11.25 8V11.25H8C7.59 11.25 7.25 11.59 7.25 12C7.25 12.41 7.59 12.75 8 12.75H11.25V16C11.25 16.41 11.59 16.75 12 16.75C12.41 16.75 12.75 16.41 12.75 16V12.75H16C16.41 12.75 16.75 12.41 16.75 12C16.75 11.59 16.41 11.25 16 11.25Z"
-			fill={`${color}`} />
-	</svg>
+	<!-- Bulk -->
 {:else if variant === 'Broken'}
-	<svg
-		width={`${size}`}
-		height={`${size}`}
-		viewBox="0 0 24 24"
-		fill="none"
-		xmlns="http://www.w3.org/2000/svg">
-		<path
-			d="M12 16V8"
-			stroke={`${color}`}
-			stroke-width="1.5"
-			stroke-linecap="round"
-			stroke-linejoin="round" />
-		<path
-			d="M14.9902 12H16.0002"
-			stroke={`${color}`}
-			stroke-width="1.5"
-			stroke-linecap="round"
-			stroke-linejoin="round" />
-		<path
-			d="M8 12H11.81"
-			stroke={`${color}`}
-			stroke-width="1.5"
-			stroke-linecap="round"
-			stroke-linejoin="round" />
-		<path
-			d="M12 16V8"
-			stroke={`${color}`}
-			stroke-width="1.5"
-			stroke-linecap="round"
-			stroke-linejoin="round" />
-		<path
-			d="M4 6C2.75 7.67 2 9.75 2 12C2 17.52 6.48 22 12 22C17.52 22 22 17.52 22 12C22 6.48 17.52 2 12 2C10.57 2 9.2 2.3 7.97 2.85"
-			stroke={`${color}`}
-			stroke-width="1.5"
-			stroke-linecap="round"
-			stroke-linejoin="round" />
-	</svg>
+	<!-- Broken -->
 {:else if variant === 'TwoTone'}
-	<svg
-		width={`${size}`}
-		height={`${size}`}
-		viewBox="0 0 24 24"
-		fill="none"
-		xmlns="http://www.w3.org/2000/svg">
-		<path
-			d="M12 22C17.5 22 22 17.5 22 12C22 6.5 17.5 2 12 2C6.5 2 2 6.5 2 12C2 17.5 6.5 22 12 22Z"
-			stroke={`${color}`}
-			stroke-width="1.5"
-			stroke-linecap="round"
-			stroke-linejoin="round" />
-		<g opacity="0.4">
-			<path
-				d="M8 12H16"
-				stroke={`${color}`}
-				stroke-width="1.5"
-				stroke-linecap="round"
-				stroke-linejoin="round" />
-			<path
-				d="M12 16V8"
-				stroke={`${color}`}
-				stroke-width="1.5"
-				stroke-linecap="round"
-				stroke-linejoin="round" />
-		</g>
-	</svg>
+	<!-- TwoTone -->
 {/if}
