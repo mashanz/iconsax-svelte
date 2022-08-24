2e9a8e2739bdc41bb40fb19b2cddc2597c7062f6
Hanjara <hanjara@landx.id>
2022-08-24 16:43:45 +0700

    [ADD] 5 item from twotone

---
+++ src/lib/MaximizeCircle.png.svelte
@@ -0,0 +1,19 @@
+<script>
+	export let size = 24;
+	export let color = '#000';
+	export let variant = 'Linear';
+</script>
+
+{#if variant === 'Linear'}
+	<!-- Linear -->
+{:else if variant === 'Outline'}
+	<!-- Outline -->
+{:else if variant === 'Bold'}
+	<!-- Bold -->
+{:else if variant === 'Bulk'}
+	<!-- Bulk -->
+{:else if variant === 'Broken'}
+	<!-- Broken -->
+{:else if variant === 'TwoTone'}
+	<!-- TwoTone -->
+{/if}
